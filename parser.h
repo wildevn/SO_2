5efe2f72734c3d404264d7e7c688a6affef79617
wildevn <45374307+wildevn@users.noreply.github.com>
2021-11-28 22:42:12 -0400

    Add files via upload

---
+++ parser.h
@@ -0,0 +1,43 @@
+#include "scanner.h"
+
+class Parser{
+
+  private:
+    Scanner* scanner;
+    Token* lToken;
+    Token* lastToken;
+    SymbolTable* globalST;
+    SymbolTable* currentST;
+    int err; // Flag de erro
+    int errSintatic; // Flag para erro sintático
+    int recToken; // Flag de troca de token
+
+    void advance();
+    int match(int, int *, int);
+    void error(int, int, string);
+    void initSymbolTable();
+    void returnST();
+
+    // Métodos da Gramatica
+    void program();
+    void mainClass();
+    void statement();
+    void expression();
+    void expressionPredict();
+    void classDeclaration();
+    void varDeclaration();
+    void methodDeclaration();
+    void type();
+    int isOP();
+    int isType();
+    int isTypeExtended();
+
+    // Tratamento da parte semântica
+    int verVariable(int, int, int);
+    int verType();
+    
+  public:
+
+    Parser(string);
+    void run();
+};
